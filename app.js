4979a4c8618705fa947e9610fe8cf953324343c6
Guan Xian <gxleow@gmail.com>
2021-02-24 12:09:25 +0800

    banner

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function parallax(element, distance, speed){
         const item = document.querySelector(element);
         item.style.transform = `translateY(${distance * speed}px)`;
